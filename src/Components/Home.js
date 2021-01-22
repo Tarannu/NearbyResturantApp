de1bcad702c8ce02a38c5aa1bc1327ea979263a1
taranum islam <tarannum.aiub@gmail.com>
2021-01-22 12:38:27 -0500

    name of resturant results are visible without error

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,10 @@ import zomato from "../api/zomato";
 
 const Home = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState({
+    id: [],
+    name: [],
+  });
 
   const handleChange = (e) => {
     setTerm(e.target.value);
@@ -17,37 +20,39 @@ const Home = () => {
       const city_id = JSON.stringify(
         city_response.data.location_suggestions[0].entity_id
       );
-      // console.log(
-      //   "web response " +
-      //     JSON.stringify(city_response.data.location_suggestions[0])
-      // );
       const food_response = await zomato.get(
         `/search?entity_id=${city_id}&entity_type=city&q=burger&sort=rating&order=asc`
       );
-      console.log("food response" + JSON.stringify(food_response.data.restaurants));
-      const items=food_response.data.restaurants.map(r=> {
-        return([r.restaurant.name, r.restaurant.id])
+      console.log(
+        "food response" + JSON.stringify(food_response.data.restaurants)
+      );
+      const items = food_response.data.restaurants; //.map(r=>{
+      //return([r.restaurant.name, r.restaurant.id])
+      //});
+      const iD = items.map((r) => r.restaurant.id);
+      console.log(iD);
+      if (items) {
+        setResults({
+          id: items.map((r) => r.restaurant.id),
+          name: items.map((r) => r.restaurant.name),
         });
-      if(items){
-      setResults(items);
-      console.log(results);
       }
-      //console.log("result item is" +JSON.stringify(results));
-      console.log(results);
+      console.log("ser result is "+results);
+      console.log("set result names are " + results.name);
     } catch (err) {
       console.log(err);
     }
-    // 
+    //
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     searchFunc(term);
-  },[term]);
-  const handleSubmit=(e)=>{
+  }, [term]);
+  const handleSubmit = (e) => {
     e.preventDefault();
-  }
-  
-
+    setTerm("");
+  };
+  //const ListItems=results.map((result)=><li key={result.id}>{result.name}</li>)
   return (
     <div style={{ padding: 50 }}>
       <SearchBar
@@ -55,11 +60,17 @@ const Home = () => {
         onTermChange={handleChange}
         onTermSubmit={handleSubmit}
       />
-      <p>The resturant name is {results} at {term}</p>
+      {results.name}
 
       <ResultSection />
     </div>
   );
 };
+// <ul>{
+//   results.map(result=>(
+//     <li key={result}>{result}</li>
+//   ))
+// }
+// </ul>
 
 export default Home;
