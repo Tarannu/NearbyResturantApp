9112df587e5bb31ca7c04cd859ad6a64ab282f20
taranum islam <tarannum.aiub@gmail.com>
2021-01-23 21:58:28 -0500

    can search with both food and location

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,21 +4,26 @@ import ResultSection from "./ResultSection";
 import zomato from "../api/zomato";
 
 const Home = () => {
-  const [term, setTerm] = useState("");
+  const [food,setFood]=useState("");
+  const [location, setLocation] = useState("");
   const [results, setResults] = useState([]);
 
-  const handleChange = (e) => {
-    setTerm(e.target.value);
+  const handleLocationChange = (e) => {
+    setLocation(e.target.value);
+    
   };
+  const handleFoodChange=(e)=>{
+    setFood(e.target.value);
+  }
 
-  const searchFunc = async (term) => {
+  const searchFunc = async (location) => {
     try {
-      const city_response = await zomato.get(`/locations?query=${term}`);
+      const city_response = await zomato.get(`/locations?query=${location}`);
       const city_id = JSON.stringify(
         city_response.data.location_suggestions[0].entity_id
       );
       const food_response = await zomato.get(
-        `/search?entity_id=${city_id}&entity_type=city&q=burger&sort=rating&order=asc`
+        `/search?entity_id=${city_id}&entity_type=city&q=${food}&sort=rating&order=asc`
       );
       console.log(
         "food response" + JSON.stringify(food_response.data.restaurants)
@@ -29,15 +34,16 @@ const Home = () => {
       const iD = items.map((r) => r.restaurant.id);
       console.log(iD);
       if (items) {
-        setResults([{
-          id: items.map((r) => r.restaurant.id),
-          image:items.map((r) => r.restaurant.name),
-          name: items.map((r) => r.restaurant.name),
-          address:items.map((r) => r.restaurant.location.address),
-          zip:items.map((r) => r.restaurant.location.zipcode),
-          city:items.map((r) => r.restaurant.location.locality_verbose),
-
-        }]);
+        setResults([
+          {
+            id: items.map((r) => r.restaurant.id),
+            image: items.map((r) => r.restaurant.name),
+            name: items.map((r) => r.restaurant.name),
+            address: items.map((r) => r.restaurant.location.address),
+            zip: items.map((r) => r.restaurant.location.zipcode),
+            city: items.map((r) => r.restaurant.location.locality_verbose),
+          },
+        ]);
       }
       console.log("ser result is " + results);
       console.log("set result names are " + results.name);
@@ -48,45 +54,40 @@ const Home = () => {
   };
 
   useEffect(() => {
-    searchFunc(term);
-  }, [term]);
+    searchFunc(location,food);
+    
+  }, [location,food]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setTerm("");
+    setLocation("");
+    setFood("");
   };
-  //const ListItems=results.map((result)=><li key={result.id}>{result.name}</li>)
+  
   return (
     <div style={{ padding: 50 }}>
       <SearchBar
-        term={term}
-        onTermChange={handleChange}
+        location={location}
+        onLocationChange={handleLocationChange}
+        onFoodChange={handleFoodChange}
         onTermSubmit={handleSubmit}
       />
       <div>
-        {results.map((result)=>
-
-          <ol className='resturaunt-list-parent'>
-            
+        {results.map((result) => (
+          <ol className="resturaunt-list-parent">
             {result.name.map((n) => (
-              <li className='resturaunt-name-list'> Name of the Resturant is {n}</li>
-              
-            ))
-
-          }
-            
-
+              <li className="resturaunt-name-list">
+                {" "}
+                Name of the Resturant is {n}
+              </li>
+            ))}
           </ol>
-        )}
+        ))}
       </div>
       <ResultSection />
     </div>
   );
 };
-// <ul>{
-//   results.map(result=>(
-//     <li key={result}>{result}</li>
-//   ))
-// }
-// </ul>
+
 
 export default Home;
